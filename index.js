6e611cc62d24e1fb2604da5adf64470b2ff29760
harrj396 <ahletmethink@gmail.com>
2022-11-01 17:27:10 -0400

    Worked on index and started adding questions

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const MarkDown = require("./utils/generateMarkdown");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input"
+        message: "What would you like to name your project?"
+        name: "Title"
+    }
+
+    {
+        type: "input"
+        message: "Describe your project"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
